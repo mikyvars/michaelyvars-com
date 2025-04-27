0ac7c58e0ac0537807dc902ab471c3c05e02ec40
Mikyvars <michaelyvars1@gmail.com>
2025-04-27 18:30:55 +0200

    fix: bad files structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "./sections/header";
-import Hero from "./sections/hero";
-import About from "./sections/about";
-import Footer from "./sections/footer";
-import Copyright from "./sections/copyright";
+import Header from "../sections/header";
+import Hero from "../sections/hero";
+import About from "../sections/about";
+import Footer from "../sections/footer";
+import Copyright from "../sections/copyright";
 
 export default function Home() {
   return (
