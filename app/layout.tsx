5e927f869307d9c95f8ac796b87d3963a7aaa086
Mikyvars <michaelyvars1@gmail.com>
2025-04-27 18:47:14 +0200

    feat: try to update the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
@@ -8,9 +8,42 @@ const interSans = Inter({
   subsets: ["latin"],
 });
 
+const title = "Michael Yvars | Étudiant développeur web";
+const description = "Juste un développeur web chill en quête d'apprentissage.";
+const url = "https://michaelyvars.com";
+
 export const metadata: Metadata = {
-  title: "Michael Yvars",
-  description: "Juste a chill web developer",
+  metadataBase: new URL(url),
+  title,
+  description,
+  keywords: ["Web Developer", "Frontend Developer", "Next.js Developer"],
+  creator: "Michael Yvars",
+  openGraph: {
+    type: "website",
+    title,
+    description,
+    url,
+    siteName: title,
+    images: [
+      {
+        url: "/images/banner-michaelyvars.jpg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+    creator: "@mikyvars",
+    images: "/images/banner-michaelyvars.jpg",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
