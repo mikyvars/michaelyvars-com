a76a7891572b4dc17e4d53c5f231c6379c65812a
Mikyvars <michaelyvars1@gmail.com>
2025-05-04 18:48:20 +0200

    feat: header logo back to top

--- sections/header.tsx
+++ sections/header.tsx
@@ -4,6 +4,7 @@ import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import Navigation from "../components/navigation";
 import NavigationMobile from "../components/navigation-mobile";
+import Link from "next/link";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +16,9 @@ const Header = () => {
   return (
     <header className="flex w-full flex-col md:flex-row">
       <div className="bg-gray-default mx-auto flex w-full max-w-7xl items-center justify-between px-4 py-4 md:px-8">
-        <p className="text-3xl font-bold text-gray-900">{"<MY />"}</p>
+        <Link href={"/"}>
+          <p className="text-3xl font-bold text-gray-900">{"<MY />"}</p>
+        </Link>
         <div onClick={handleClick} className="md:hidden">
           {isOpen ? <X /> : <Menu />}
         </div>
